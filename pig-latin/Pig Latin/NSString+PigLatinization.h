177a57a1b922b4722930875da72ffcf069b02b96
Willow Bumby <willow.rose@bumby.ca>
2016-07-10 14:04:30 -0600

    Add assignment 'Pig Latin' - Week 1, Day X

---
+++ pig-latin/Pig Latin/NSString+PigLatinization.h
@@ -0,0 +1,15 @@
+//
+//  NSString+PigLatinization.h
+//  Pig Latin
+//
+//  Created by Willow Belle on 2015-10-23.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PigLatinization)
+
++ (NSString *)stringByPigLatinization:(NSString *)englishString;
+
+@end
