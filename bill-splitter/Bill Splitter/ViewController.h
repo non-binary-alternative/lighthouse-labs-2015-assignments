29d60e3e1ec03b3359ecba154f16c31b8258919e
Willow Bumby <willow.rose@bumby.ca>
2016-07-10 15:08:57 -0600

    Add assignment 'Bill Splitter' - Week 2, Day 6

---
+++ bill-splitter/Bill Splitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Bill Splitter
+//
+//  Created by Willow Belle on 2015-10-31.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@end
+
