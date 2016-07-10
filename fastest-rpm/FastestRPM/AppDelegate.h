64cdb2e905467ba7b50ea446051f07384c6e110e
Willow Bumby <willow.rose@bumby.ca>
2016-07-10 14:47:38 -0600

    Add assignment 'Fastest RPM' - Week 2, Day 4

---
+++ fastest-rpm/FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Willow Belle on 2015-10-29.
+//  Copyright © 2015 Cosmic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
